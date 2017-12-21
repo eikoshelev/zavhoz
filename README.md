00f274e5591a79abe781ae89c2ac6621688d9d7f
Evgeny Koshelev <yikoshe1@mts.ru>
2017-12-21 13:12:03 +0300

    initial version of the golang script for couchbase server

---
+++ README.md
@@ -0,0 +1,4 @@
+# repo
+
+cb.go - initial version of the script for couchbase server on the golang with "upsert" and "get" methods.
+
---
+++ cb.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/couchbase/gocb"
+)
+
+// create structur
+type Data struct {
+
+	IP  string  `json:"ip"`
+	App  []string  `json:"apps"`
+	Active  bool  `json:"active"`
+
+}
+
+func main() {
+	// connect to cluster
+	cluster, _ := gocb.Connect("127.0.0.1:8091")
+	cluster.Authenticate(gocb.PasswordAuthenticator{
+		Username: "Admin",
+		Password: "aadmin",
+	})
+
+	bucket, _ := cluster.OpenBucket("testbucket", "")
+
+	// working with bucket
+	// bucket.Manager(...)
+
+	// added new data
+	bucket.Upsert("test_data",
+
+		Data{
+
+			IP: "0.0.0.0",
+			App: []string{"foo", "bar"},
+			Active: true,
+
+			}, 0)
+
+	// get data
+	var inData Data
+	bucket.Get("test_data", &inData)
+	fmt.Println("Data: ", inData)
+}
