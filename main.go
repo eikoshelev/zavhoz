25b6e7538ba5104d2f0c59ab68e6b5c338aae670
Evgeny Koshelev <yikoshe1@mts.ru>
2018-01-12 14:13:27 +0300

    upd if err != nil

--- main.go
+++ main.go
@@ -2,31 +2,80 @@ package main
 
 import (
 
-	//"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"io/ioutil"
 	"os/signal"
 	"syscall"
 
+	"github.com/go-yaml/yaml"
 	"github.com/couchbase/gocb"
 	"github.com/miekg/dns"
 )
 
 type inventory struct {
-	IP     string   `json:"ip"`
-	Apps   []string `json:"apps"`
-	Active bool     `json:"active"`
+	IP     string `json:"ip"`
+	Apps []string `json:"apps"`
+	Active bool   `json:"active"`
 }
 
-var conn, _ = gocb.Connect("127.0.0.1:8091")
-var cluster = conn.Authenticate(gocb.PasswordAuthenticator{Username: "Admin", Password: "aadmin"})
-var bucket, _ = conn.OpenBucket("testbucket", "")
+type conf struct {
+	
+	Server struct {
+		Port    string `yaml:"port"`
+		Network string `yaml:"network"`
+		Ttl     uint32 `yaml:"ttl"`
+	} `yaml:"server"`
+
+	Storage struct {
+		Login    string `yaml:"login"`
+		Password string `yaml:"password"`
+		Bucket   string `yaml:"bucket"`
+		Hosts  []string `yaml:"hosts"`
+	} `yaml:"storage"`
+}
+
+var configFlag = flag.String("config", "./default.yaml", "set config file in the yaml format")
+var config conf
+
+func configure() conf {
+
+	configFile, err := ioutil.ReadFile(*configFlag)
+	if err != nil {
+    	fmt.Println(err)
+	}
+
+    var c conf
+	err = yaml.Unmarshal(configFile, &c)
+	if err != nil {
+		fmt.Println("can't unmarshal ", *configFlag, err)
+	}
+
+	return c
+}
+
+var bucket *gocb.Bucket
 
 func main() {
 
-	server := &dns.Server{Addr: ":51", Net: "udp"}
+	flag.Parse()
+	config = configure()
+	fmt.Printf("%+v", config)
+	
+	conn, err := gocb.Connect(config.Storage.Hosts[0])
+	if err != nil {
+    	fmt.Println("can't connect to ", *conn, err)
+	}
+	_ = conn.Authenticate(gocb.PasswordAuthenticator{config.Storage.Login, config.Storage.Password})
+	bucket, err = conn.OpenBucket(config.Storage.Bucket, "")
+	if err != nil {
+    	fmt.Println("can't open bucket ", *bucket, err)
+	}
+
+	server := &dns.Server{Addr: ":" + config.Server.Port, Net: config.Server.Network}
 
 	go func() {
 		if err := server.ListenAndServe(); err != nil {
@@ -64,11 +113,10 @@ func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 		}
 
 		answer := new(dns.A)
-		answer.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 3600}
-		answer.A = net.ParseIP(host.IP) // Preference = 10
+		answer.Hdr = dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: config.Server.Ttl}
+		answer.A = net.ParseIP(host.IP)
 		m.Answer = append(m.Answer, answer)
 	}
-
 	m.SetReply(r)
 	fmt.Printf("%+v\n", m)
 	w.WriteMsg(m)
